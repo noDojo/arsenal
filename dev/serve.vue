f42ab19e6d2952c5cf01d78420008c0bc21ee4c4
noDojo <nullDojo@gmail.com>
2021-01-05 22:45:28 -0500

    Initialize project

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import { ArsenalSample } from '@/entry';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    ArsenalSample,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <arsenal-sample />
+  </div>
+</template>
